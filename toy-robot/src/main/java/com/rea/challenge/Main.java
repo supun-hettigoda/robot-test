3b09b436c030a94477144025662607623473de9c
Supun <hettigoda.supun@gmail.com>
2020-10-15 23:35:00 +1100

    add logging configurations for self4j facade with log4j provider and update the gitignore file to exclude logs folder.

--- toy-robot/src/main/java/com/rea/challenge/Main.java
+++ toy-robot/src/main/java/com/rea/challenge/Main.java
@@ -1,6 +1,9 @@
 
 package com.rea.challenge;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * This is the main class will start the RobotSimulation application on standard input (command
  * line).
@@ -8,8 +11,12 @@ package com.rea.challenge;
  */
 public class Main
 {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args)
     {
         // TODO implement me.
+        LOGGER.info("Initializing Robot Simulator.");
+        LOGGER.debug("Initializing Robot Simulator in DEBUG mode.");
     }
 }
