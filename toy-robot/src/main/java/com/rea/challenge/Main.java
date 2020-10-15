9fb8212ca1abb9219280d3f8e595032348d22872
Supun <hettigoda.supun@gmail.com>
2020-10-15 22:09:32 +1100

    adding initial maven based project structure with basic dependencies.

---
+++ toy-robot/src/main/java/com/rea/challenge/Main.java
@@ -0,0 +1,15 @@
+
+package com.rea.challenge;
+
+/**
+ * This is the main class will start the RobotSimulation application on standard input (command
+ * line).
+ *
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // TODO implement me.
+    }
+}
