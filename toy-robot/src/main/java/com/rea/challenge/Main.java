e4c39b70244a0b8650f388f6401e9aff43605169
Supun <hettigoda.supun@gmail.com>
2020-10-16 18:05:12 +1100

    rename the existing auto generated AppTest class to AppInitializationTest with initial test cases for a successfull app initialisation expecations.

--- toy-robot/src/main/java/com/rea/challenge/Main.java
+++ toy-robot/src/main/java/com/rea/challenge/Main.java
@@ -5,18 +5,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This is the main class will start the RobotSimulation application on standard input (command
- * line).
- *
+ * This main class will start the RobotSimulation application on standard input (command line).
  */
 public class Main
 {
     public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
+    public static final String MSG_SIMULATION_ACTIVE = "REA Robot Simulation Up And Running.";
+
     public static void main(String[] args)
     {
-        // TODO implement me.
         LOGGER.info("Initializing Robot Simulator.");
         LOGGER.debug("Initializing Robot Simulator in DEBUG mode.");
+
+        // TODO initialise the application.
+
+        // out put a message to user on successful initialisation.
+        LOGGER.debug("Robot Simulator successfully initialised.");
+        System.out.println(MSG_SIMULATION_ACTIVE);
     }
 }
