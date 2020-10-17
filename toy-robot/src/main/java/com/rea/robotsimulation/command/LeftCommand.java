34a403ac4cd23605301a581d1f8e8bff3fa78d87
Supun <hettigoda.supun@gmail.com>
2020-10-17 23:31:21 +1100

    Implement the left command.

--- toy-robot/src/main/java/com/rea/robotsimulation/command/LeftCommand.java
+++ toy-robot/src/main/java/com/rea/robotsimulation/command/LeftCommand.java
@@ -3,14 +3,15 @@ package com.rea.robotsimulation.command;
 
 import com.rea.robotsimulation.robot.Robot;
 
+/**
+ * Executable command to rotate the {@code Robot} left.
+ *
+ */
 public class LeftCommand implements ExecutableRobotCommand
 {
-
     @Override
     public void execute(Robot robot)
     {
-        // TODO implement me
-
+        robot.rotateLeft();
     }
-
 }
