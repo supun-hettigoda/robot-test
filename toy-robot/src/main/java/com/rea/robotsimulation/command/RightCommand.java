8121cf7a4b26a54aebd6d9a64d91ee50edbd3175
Supun <hettigoda.supun@gmail.com>
2020-10-17 23:33:56 +1100

    Implement the right command.

--- toy-robot/src/main/java/com/rea/robotsimulation/command/RightCommand.java
+++ toy-robot/src/main/java/com/rea/robotsimulation/command/RightCommand.java
@@ -3,14 +3,15 @@ package com.rea.robotsimulation.command;
 
 import com.rea.robotsimulation.robot.Robot;
 
+/**
+ * Executable command to rotate the {@code Robot} right.
+ *
+ */
 public class RightCommand implements ExecutableRobotCommand
 {
-
     @Override
     public void execute(Robot robot)
     {
-        // TODO implement me
-
+        robot.rotateRight();
     }
-
 }
---
+++ toy-robot/src/test/java/com/rea/robotsimulation/command/test/RightCommandTest.java
@@ -0,0 +1,35 @@
+
+package com.rea.robotsimulation.command.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.rea.robotsimulation.command.RightCommand;
+import com.rea.robotsimulation.robot.Robot;
+
+/**
+ * This class contains test cases for {@link RightCommand}.
+ *
+ */
+public class RightCommandTest
+{
+    private Robot robot;
+    private RightCommand command;
+
+    @Before
+    public void setUp()
+    {
+        command = new RightCommand();
+        robot = Mockito.mock(Robot.class);
+    }
+
+    @Test
+    public void test_execute()
+    {
+        // when execute we expect Robot's rotateRight() method to be invoked.
+        Mockito.doNothing().when(robot).rotateRight();
+        command.execute(robot);
+        Mockito.verify(robot).rotateRight();
+    }
+}
