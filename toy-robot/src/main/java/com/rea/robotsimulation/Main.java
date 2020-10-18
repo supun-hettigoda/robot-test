c71bd5db214612f741ab634e4c513aa343c4552e
Supun <hettigoda.supun@gmail.com>
2020-10-18 23:31:37 +1100

    Hook up the RobotSimulation to the Main so it can start the simulation process on console by default.

--- toy-robot/src/main/java/com/rea/robotsimulation/Main.java
+++ toy-robot/src/main/java/com/rea/robotsimulation/Main.java
@@ -4,6 +4,12 @@ package com.rea.robotsimulation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.rea.robotsimulation.robot.OneStepAheadGridScanner;
+import com.rea.robotsimulation.robot.RobotTableTop;
+import com.rea.robotsimulation.robot.SelfMovingRobot;
+import com.rea.robotsimulation.simulator.InputStreamCommandProvider;
+import com.rea.robotsimulation.simulator.RobotSimulator;
+
 /**
  * This main class will start the RobotSimulation application on standard input (command line).
  */
@@ -11,17 +17,19 @@ public class Main
 {
     public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    public static final String MSG_SIMULATION_ACTIVE = "REA Robot Simulation Up And Running.";
+    public static final String MSG_SIMULATION_ACTIVE =
+            "Robot Simulation waiting for your commands, press Ctrl-D to terminate";
 
     public static void main(String[] args)
     {
         LOGGER.info("Initializing Robot Simulator.");
         LOGGER.debug("Initializing Robot Simulator in DEBUG mode.");
 
-        // TODO initialise the application.
-
-        // out put a message to user on successful initialisation.
-        LOGGER.debug("Robot Simulator successfully initialised.");
+        RobotSimulator robotSimulator =
+                new RobotSimulator(new InputStreamCommandProvider(System.in), System.out::println);
+        LOGGER.info("Simulator running...");
         System.out.println(MSG_SIMULATION_ACTIVE);
+        robotSimulator
+                .start(new SelfMovingRobot(new OneStepAheadGridScanner(new RobotTableTop(5, 5))));
     }
 }
