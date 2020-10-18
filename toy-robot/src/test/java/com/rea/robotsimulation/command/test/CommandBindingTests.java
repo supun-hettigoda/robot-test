85924beba72c5a1e6d7ac72d3e27d668931ebabd
Supun <hettigoda.supun@gmail.com>
2020-10-18 12:25:56 +1100

    Adding CommandBinding class which defines all possible commands for the application and methods to bind the executables for those.

--- toy-robot/src/test/java/com/rea/robotsimulation/command/test/CommandBindingTests.java
+++ toy-robot/src/test/java/com/rea/robotsimulation/command/test/CommandBindingTests.java
@@ -1,11 +1,28 @@
 
 package com.rea.robotsimulation.command.test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
 import com.rea.robotsimulation.command.CommandBinding;
+import com.rea.robotsimulation.command.ExecutableRobotCommand;
+import com.rea.robotsimulation.command.LeftCommand;
+import com.rea.robotsimulation.command.MoveCommand;
+import com.rea.robotsimulation.command.PlaceCommand;
+import com.rea.robotsimulation.command.ReportCommand;
+import com.rea.robotsimulation.command.RightCommand;
+import com.rea.robotsimulation.util.Pair;
 
 /**
  * This class contains unit tests for {@link CommandBinding}.
@@ -14,16 +31,48 @@ import com.rea.robotsimulation.command.CommandBinding;
 @RunWith(Theories.class)
 public class CommandBindingTests
 {
+    @DataPoints("notParsableInputs")
+    public static String[] notParsableInputs = new String[]
+    {
+        // @formatter:off
+        null,
+        "",
+        "place 0,0,NORTH",
+        "rigth",
+        "RE PORT",
+        "RIGHT 1",
+        "PLACE",
+        "left",
+        "DAMN"
+        // @formatter:on
+    };
 
     @Theory
-    public void test_toExecutable()
+    public void test_commandReturnsForValidInputs(
+            @FromDataPoints("validInputsAndCommand") Pair<String, Class<? extends ExecutableRobotCommand>> testData)
     {
-
+        Optional<ExecutableRobotCommand> actual = CommandBinding.toExecutable(testData.getFirst());
+        assertThat(actual, instanceOf(Optional.class));
+        assertThat(actual.get(), instanceOf(testData.getSecond()));
     }
 
-//    @DataPoint("matchedParsers")
-//    public List<Pair<ReportCommandParser, String>> matchedParsers()
-//    {
-////        return Arrays.asList(Pair.of(Mockito.mock(ExecutableRobotCommand.class), second));
-//    }
+    @Theory
+    public void
+            test_noCommandReturnForInvalidInputs(@FromDataPoints("notParsableInputs") String input)
+    {
+        Optional<ExecutableRobotCommand> actual = CommandBinding.toExecutable(input);
+        assertEquals(Optional.empty(), actual);
+    }
+
+    @DataPoints("validInputsAndCommand")
+    public static List<Pair<String, Class<? extends ExecutableRobotCommand>>>
+            validInputsAndCommand()
+    {
+        return Arrays.asList(
+                Pair.of("MOVE", MoveCommand.class),
+                Pair.of("LEFT", LeftCommand.class),
+                Pair.of("REPORT", ReportCommand.class),
+                Pair.of("PLACE 1,2,NORTH", PlaceCommand.class),
+                Pair.of("RIGHT", RightCommand.class));
+    }
 }
