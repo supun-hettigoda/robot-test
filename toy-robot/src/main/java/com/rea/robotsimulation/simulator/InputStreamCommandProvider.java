bb1d2db77932d27334ea7ebd78ee128e4826009d
Supun <hettigoda.supun@gmail.com>
2020-10-18 16:14:02 +1100

    Added simulation package with InputCommandProvider interface and it's implementation for provide inputs from a given InputStream.

---
+++ toy-robot/src/main/java/com/rea/robotsimulation/simulator/InputStreamCommandProvider.java
@@ -0,0 +1,43 @@
+
+package com.rea.robotsimulation.simulator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Optional;
+
+/**
+ * Implementation of {@link InputCommandProvider} which reads the commands from {@link InputStream}
+ * of input.
+ */
+public class InputStreamCommandProvider implements InputCommandProvider
+{
+    private BufferedReader reader;
+
+    /**
+     * Input stream to read command from
+     *
+     * @param inputStream the input stream
+     */
+    public InputStreamCommandProvider(InputStream inputStream)
+    {
+        reader = new BufferedReader(new InputStreamReader(inputStream));
+    }
+
+    @Override
+    public Optional<String> getNextCommand()
+    {
+        try
+        {
+            // TIDY can we get rid of the unchecked style here
+            // using functional interface conversion?
+            return Optional.ofNullable(reader.readLine());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+}
